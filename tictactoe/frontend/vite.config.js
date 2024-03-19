da18e56ff1c0e24dcd66122fc202d82e413ea193
Nathan Self <nwself@cs.vt.edu>
2024-03-18 22:48:26 -0400

    Add remove outdated asset files as part of build process

--- tictactoe/frontend/vite.config.js
+++ tictactoe/frontend/vite.config.js
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from 'path'
+import { rm } from 'node:fs/promises'
+
+
+const outDir = "../templates";
+const assetsDir = "static/assets";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "Cleaning assets folder",
+      async buildStart() {
+        // console.log(await readdir(resolve(__dirname, outDir, assetsDir), { recursive: true, force: true }));
+        await rm(resolve(__dirname, outDir, assetsDir), { recursive: true, force: true });
+      }
+    },
+  ],
   build: {
-    outDir: "../templates",
-    assetsDir: "static/assets",
+    outDir,
+    assetsDir,
     emptyOutDir: false,
   }
 })
